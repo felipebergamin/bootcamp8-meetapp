8164af8397a8322dc72a296f7837c7319f065d0d
Felipe Bergamin <felipebergamin6@gmail.com>
2019-08-29 16:13:26 -0300

    refact. routes configuration

--- src/routes.js
+++ src/routes.js
@@ -19,9 +19,15 @@ router.post('/auth', TokenController.store);
 router.use(authMiddleware);
 router.put('/users', UserController.update);
 router.post('/files', upload.single('file'), FileController.store);
-router.post('/meetups', MeetupController.store);
-router.get('/meetups', MeetupController.index);
-router.put('/meetups/:meetupId', MeetupController.update);
-router.delete('/meetups/:meetupId', MeetupController.delete);
+
+router
+  .route('/meetups')
+  .post(MeetupController.store)
+  .get(MeetupController.index);
+
+router
+  .route('/meetups/:meetupId')
+  .put(MeetupController.update)
+  .delete(MeetupController.delete);
 
 export default router;
